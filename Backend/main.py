0da1d03a176e18ad4474b63bd21baaf2dc5d33fd
yardenDe <yardendeshe@gmail.com>
2025-08-26 19:06:31 +0300

    Added temporary dataset, connected to Supabase and opened initial stream

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,52 @@
 from fastapi import FastAPI, Body
 import psycopg2
 from openai import OpenAI
+import requests
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+response = client.chat.completions.create(
+    model="",
+    input = 
+)
+
+# Temporary CSV path for demonstration purposes
+cvs_path = "C:\\Users\\User\\Personal_Projects\\NutriAI\\SQL\\supplements_tamp.csv"
+data = pd.read_csv(cvs_path)
+
+if not os.path.exists(csv_path):
+    raise FileNotFoundError(f"File not found in path: {csv_path}")
+
+data = pd.read_csv(csv_path)
+print( " Read {len(data)} from CSV file successfully")
+
+# Connect to Supabase PostgreSQL
+conn = psycopg2.connect(
+    dbname="postgres",                   
+    user="postgres",                    
+    password="zurqub-mecbof-7Dexxi",   
+    host="db.ailnvqajkzfxdeipdvdy.supabase.co",           
+    port="5432",
+    sslmode="require"
+)
+cur = conn.cursor()
+
+for idx, row in data.iterrows():
+    name = row["name"]
+    desc = row["description"]
+    cur.execute(""" SELECT add_supplements(%s, %s, %s); """, (name, desc, None))   
+    print(f"Inserted {name} into the database.")
+
+conn.commit()
+cur.close()
+conn.close()
+
 
 app = FastAPI()
 
